88211e9c7f51c343036efce759d3854eff91aa2e
Kethan Vegunta <kethan@vegunta.com>
2021-01-18 11:15:59 -0800

    add table of types

--- array_data_structure.py
+++ array_data_structure.py
@@ -1,6 +1,24 @@
 from array import *
 array1 = array('i', [1, 2, 3])
 
+# Array Types:
+# Typecode |                  Value 
+# ---------|-------------------------------------|
+#     b    |   signed integer size 1 bytes/td>   |
+# ---------|-------------------------------------|
+#     B    |   unsigned integer of size 1 byte   |
+# ---------|-------------------------------------|
+#     c    |      character of size 1 byte       |
+# ---------|-------------------------------------|
+#     i    |   signed integer of size 2 bytes    |
+# ---------|-------------------------------------|
+#     I    |   unsigned integer of size 2 bytes  |
+# ---------|-------------------------------------|
+#     f    |   floating point of size 4 bytes    |
+# ---------|-------------------------------------|
+#     d    |   floating point of size 8 bytes    |
+# ---------|-------------------------------------|
+
 array1[1] = -1
 print(array1[1])
 print(array1.index(1))
