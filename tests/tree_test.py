485ae74a07cea0fcb08aeeb30fa23e537545ce0d
Kethan Vegunta <kethan@vegunta.com>
2021-06-06 16:03:34 -0700

    add testing for tree

--- tests/tree_test.py
+++ tests/tree_test.py
@@ -8,34 +8,16 @@ sys.path.insert(0, parentdir)
 
 from tree_data_structure import Node
 
-root = Node("Languages Good For")
-root.insert("Serverside Web Development")
-root.insert("For Speed")
-root[0].insert("Python")
-root[0].insert("NodeJS")
-root[0].insert("Ruby")
-root[0].insert("PHP")
-root[0].insert("C")
-root[0].insert("C++")
-root[0].insert("Java")
-root[0].insert("C#")
-root[0][0].insert("Flask")
-root[0][0].insert("Django")
-root[0][2].insert("Ruby on Rails")
-root[0][0][0].insert(1.1)
-root[0][0][0].insert(2.0)
-root[0][0][0][0].insert("1.0.1")
-root[0][0][0][1].insert("2.0.1")
-root[0][0][0][1].insert("2.0.0")
+root = Node("Udo")
+root.insert("Marc")
+root[0].insert("Lian")
+root[0][0].insert("Lian")
+root.insert("Dan")
+root[1].insert("Jet")
+root[1].insert("Jan")
+root[1].insert("Joe")
+root.insert("Joe")
 
-# if not str(root) == "                               Languages Good For\n                     Serverside Web Development                         For Speed\n          Python             NodeJS      Ruby  PHP  C  C++  Java  C#  \n       Flask         Django  Ruby on Rails    \n 1.1       2.0             \n1.0.1  2.0.1  2.0.0        ":  raise ValueError
+if not str(root) == "Udo\n├── Marc\n│   └── Lian\n│       └── Lian\n├── Dan\n│   ├── Jet\n│   ├── Jan\n│   └── Joe\n└── Joe":  raise ValueError
+
 print(root)
-"""
-Udo
-├── Marc
-│   └── Lian
-└── Dan
-    ├── Jet
-    ├── Jan
-    └── Joe
-"""
